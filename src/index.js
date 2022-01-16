ea2ca12a134f1de7bf753bf2d1b635f34bdf9d5b
Dannyroohm <dannyrooh@hotmail.com>
2022-01-16 09:23:45 -0300

    add gitignore

--- src/index.js
+++ src/index.js
@@ -3,18 +3,46 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+
+    constructor(props){
+        super(props)
+        this.state = {
+            value: null
+        }
+    }
+
     render() {
       return (
-        <button className="square">
-           {this.props.valueCeil * 2}
+        <button className="square" 
+                onClick={ () => this.props.onClick() }>
+           {this.props.value}
         </button>
       );
     }
   }
   
   class Board extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            squares: Array(9).fill(null)
+        }
+    }
+
+
+    handleClick(i){
+        const squares = this.state.squares.slice()
+        squares[i] = 'X'
+        this.setState({squares: squares})
+    }
+
     renderSquare(i) {
-      return <Square valueCeil={i} />;
+      return (
+        <Square 
+            value={this.state.squares[i]} 
+            onClick={ () => this.handleClick(i)}
+        />)
     }
   
     render() {
@@ -63,4 +91,6 @@ class Square extends React.Component {
   
   ReactDOM.render(
     <Game />,
-    document.get
+    document.getElementById('root')
+  );
+  
